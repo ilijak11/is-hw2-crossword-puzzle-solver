df3524623f45f79285ae7fac90cddbb6b5ddc04e
alajdza <garfildk007@gmail.com>
2022-12-22 18:03:34 +0100

    backtracking works on example 1

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import sys
 import game
 import algorithms
+import copy
 
 
 def backtrack():
@@ -47,53 +48,119 @@ class BackTracking(algorithms.Algorithm):
             while j < len(vars):
                 var2 = vars[j]
 
+                if len(fields1) == len(variable_fields[var2]):
+                    constraints[var1].append({'type': 0, 'var': var2})
+                    constraints[var2].append({'type': 0, 'var': var1})
+
                 for ind, field in enumerate(variable_fields[var2]):
                     if field in fields1:
-                        constraints[var1].append({'var': var2, 'var_index': ind, 'my_ind': fields1.index(field)})
-                        constraints[var2].append({'var': var1, 'var_index': fields1.index(field), 'my_ind': ind})
+                        constraints[var1].append({'type': 1, 'var': var2, 'var_ind': ind, 'my_ind': fields1.index(field)})
+                        constraints[var2].append({'type': 1, 'var': var1, 'var_ind': fields1.index(field), 'my_ind': ind})
                         
                 j = j + 1
 
         return constraints
 
-    def is_consistent(self):
+    def build_domains(self, variables, words):
+        domains = {}
+        # domain entry
+        for var, length in variables.items():
+            domains[var] = [word for word in words if len(word) == length]
+        
+        return domains
+
+
+    def is_consistent(self, sel_var, sel_val, vars, domains, constraints):
+        #var - selected variable
+        #val - potential variable value
+        #domains - other variable domains
+        #constraints - variable constraints
+
+        for constraint in constraints[sel_var]:
+
+            #one word multiple times
+
+            # if constraint['type'] == 0:
+            #     #cannot be the same
+            #     if domains[constraint['var']]['val'] == sel_val:
+            #         print('VAL: ', sel_val, ' already taken VAR: ', constraint['var'])
+            #         return False 
+
+            if constraint['type'] == 1:
+                #intersecting
+                #if they hawe different chars at intersecting fields
+                comparing_var_val = vars[constraint['var']]
+                if comparing_var_val is not None:
+                    my_ind = constraint['my_ind']
+                    var_ind = constraint['var_ind']
+                    if comparing_var_val[var_ind] != sel_val[my_ind]:
+                        print('VAL: ', sel_val, ' my_ind: ', my_ind, ' and VAL: ', comparing_var_val, ' var_ind: ', var_ind,  'in VAR: ', constraint['var'], ' dont match')
+                        return False
+
         return True
 
-    def backtrack(self, vars, lvl, domains, solution):
-        if lvl == len(vars):
+    def backtrack(self, vars, words, curr_var_ind, domains, constraints, solution, var_values):
+        if curr_var_ind == len(vars):
+            print("END")
+            print("---------------------")
             return True
 
-        var = vars[lvl]
+        var = list(vars.keys())[curr_var_ind]
 
-        for val in domains[var]:
-            if self.is_consistent():
-                solution.append([var, val[0], domains])
+        print("---------------------")
+        print("VAR: ", var)
 
+        for ind, val in enumerate(domains[var]):
+            print("VAR: ", var)
+            if self.is_consistent(var, val, vars, domains, constraints):
+                print("     VAL: ", val, " OK")
+                solution.append([var, ind, domains])
+                copied_domains = copy.deepcopy(domains)
+                copied_vars = copy.deepcopy(vars)
+                copied_domains[var] = [val]
+                copied_vars[var] = val
+                var_values[var] = val
+                if self.backtrack(copied_vars, words, curr_var_ind+1, copied_domains, constraints, solution, var_values):
+                    print("STEP UP")
+                    print("---------------------")
+                    return True
+            else:
+                print("     VAL: ", val, " NOT OK")
         
         #backtrack
         solution.append([var, None, domains])
+        print("BACKTRACK")
+        print("---------------------")
         return False
 
     def get_algorithm_steps(self, tiles, variables, words):
 
-        print(variables)
-        ret = self.build_constraints(tiles)
-        print(ret)
-        #
-        domains = {}
-        # domain entry
-        # var : [(ind, word)...]
-        for var, length in variables.items():
-            domains[var] = [(ind, word) for ind, word in enumerate(words) if len(word) == length]
+        #build domains:
+        domains = self.build_domains(variables, words)
+        print("Domains")
+        #print(domains)
+        for var, domain in domains.items():
+            print(var, " ", domain)
 
-        lvl = 0
+        #build constraints:
+        constraints = self.build_constraints(tiles)
+        print("Constraints")
+        #print(constraints)
+
+        curr_var_ind = 0
         solution = []
+        var_values = {var: None for var in variables.keys()}
 
-        #build constraints dir
+        #call backtrack algorithm
 
-        self.backtrack(list(variables.keys()), lvl, domains, solution)
+        print("Backtrack START")
+        self.backtrack({var: None for var in variables.keys()}, words, curr_var_ind, domains, constraints, solution, var_values)
+        print("Backtrack END")
 
-        #print(solution)
+        print("STEPS")
+        print(solution)
+        print("SOLUTON")
+        print(var_values)
 
         #print(domains)
 
